a44feaa2b4b14e7200199b23587792b29d785fd5
Aleksandr Smirnov <alvonrims@gmail.com>
2021-11-02 11:31:04 +0300

    - replace MDR32F9Qx_config.h ver.1.3.0 on ver.2.0.2 - remove from project MDR32F9Qx_board.h - set HSE_Value = 16 MHz (use to LDM board) - add support library version 2.0.2

--- config/MDR32F9Qx_config.h
+++ config/MDR32F9Qx_config.h
@@ -1,9 +1,9 @@
 /**
   ******************************************************************************
-  * @file    MDR32F9Qx_Demo\Project\Demo\MDR32F9Qx_config.h
+  * @file    MDR32F9Qx_config.h
   * @author  Milandr Application Team
-  * @version V1.2.0
-  * @date    10/07/2010
+  * @version V2.0.2
+  * @date    22/09/2021
   * @brief   Library configuration file.
   ******************************************************************************
   * <br><br>
@@ -14,139 +14,243 @@
   * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
   * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
-  * <h2><center>&copy; COPYRIGHT 2011 Milandr</center></h2>
+  * <h2><center>&copy; COPYRIGHT 2021 Milandr</center></h2>
   */
 
 /**
- * @mainpage MDR32F9Qx Standard Peripherals Library.
- * MDR32F9Qx Standard Peripherals Library is a package consisting of
- * all standard peripheral device drivers for 1986BE9x, K1986BE9x, MDR32F9Qx
- * and 1986VE1T microcontrollers.
- * This library is a firmware package which contains a collection of routines,
- * data structures and macros covering the features of Milandr MDR32F9Qx
- * peripherals. It includes a description of the device drivers plus a set of
- * examples for each peripheral. The firmware library allows any device to be
- * used in the user application without the need for in-depth study of each
- * peripherals specifications. Using the Standard Peripherals Library has two
- * advantages: it saves significant time that would otherwise be spent in
- * coding, while simultaneously reducing application development and
- * integration costs.
- *
- * The MDR32F9Qx Standard Peripherals Library is compatible with Milandr
- * 1986BE9x evaluation boards and Milandr evaluation board for MC 1986VE1T
- * (EVAL 22.0 B).
- *
- * The MDR32F9Qx Standard Peripherals Library is full CMSIS compliant.
- */
+  * @mainpage MDR32Fx Standard Peripherals Library.
+  * MDR32Fx Standard Peripherals Library is a package consisting of
+  * all standard peripheral device drivers for 1986VE9x, K1986VE9x, MDR32F9Qx,
+  * 1986VE1T, 1986VE3T, 1901VC1T microcontrollers.
+  * This library is a firmware package which contains a collection of routines,
+  * data structures and macros covering the features of Milandr MDR32Fx
+  * peripherals. It includes a description of the device drivers plus a set of
+  * examples for each peripheral. The firmware library allows any device to be
+  * used in the user application without the need for in-depth study of each
+  * peripherals specifications. Using the Standard Peripherals Library has two
+  * advantages: it saves significant time that would otherwise be spent in
+  * coding, while simultaneously reducing application development and
+  * integration costs.
+  *
+  * The MDR32Fx Standard Peripherals Library is compatible with Milandr
+  * 1986BE9x evaluation boards, Milandr evaluation board for MC 1986VE1T
+  * (EVAL 22.0 B) and evaluation board for MC 1986VE3T.
+  *
+  * The MDR32Fx Standard Peripherals Library is full CMSIS compliant.
+  */
+
+// <<< Use Configuration Wizard in Context Menu >>>
+
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MDR32F9Qx_CONFIG_H
 #define __MDR32F9Qx_CONFIG_H
 
-#include "MDR32F9Qx_board.h"
-#include "MDR32F9Qx_lib.h"
-#include "stdint.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#if defined (USE_MDR32F9Q1_Rev0) || defined (USE_MDR32F9Q1_Rev1) ||\
-    defined (USE_MDR32F9Q2_Rev0) || defined (USE_MDR32F9Q2_Rev1) ||\
-    defined (USE_MDR32F9Q3_Rev0) || defined (USE_MDR32F9Q3_Rev1) ||\
-    defined (USE_MDR1986VE94)
-	#define USE_MDR1986VE9x
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include <RTE_Components.h> // Keil uVision specific inclusion
+
+/* Uncomment the line corresponding to the target microcontroller */
+/* When using Keil uVision, the definition is set automatically
+   (when RTE_Components.h included) according to the selected
+   microcontroller in "Options for Target->Device" window.
+   For MDR1986VE1T and MDR1986VE3T revision is selected in 
+   "Device->Startup" variant in "Manage Run-Time Environment" window */
+// #define USE_MDR1986VE91
+#define USE_MDR1986VE92
+// #define USE_MDR1986VE93
+// #define USE_MDR1986VE94
+// #define USE_MDR1986VE1T_REV3_4
+// #define USE_MDR1986VE1T_REV6
+// #define USE_MDR1986VE3T_REV2
+// #define USE_MDR1986VE3T_REV3
+// #define USE_MDR1901VC1T
+
+
+#if defined (USE_MDR1986VE91) || defined (USE_MDR1986VE92) || defined (USE_MDR1986VE93) || defined (USE_MDR1986VE94)
+    #undef USE_MDR1986VE9x
+    #define USE_MDR1986VE9x
+#endif
+
+/* Select the header file for target microcontroller */
+#if defined (USE_MDR1986VE9x)
+    #include "MDR32Fx.h"
+#elif defined (USE_MDR1986VE1T_REV3_4) || defined (USE_MDR1986VE1T_REV6)
+    #define USE_MDR1986VE1T
+    #include "MDR1986VE1T.h"
+#elif defined (USE_MDR1986VE3T_REV2) || defined (USE_MDR1986VE3T_REV3)
+    #define USE_MDR1986VE3
+    #include "MDR1986VE3.h"
+#elif defined (USE_MDR1901VC1T)
+    #include "MDR1901VC1T.h"
+#else
+    #error "Microcontroller not selected in MDR32F9Qx_config.h file"
 #endif
 
 
-/* Selet the header file for target microcontroller */
-#if defined ( USE_MDR1986VE9x )
-	#include "MDR32Fx.h"
-#elif defined (USE_MDR1986VE1T)
-	#include "MDR1986VE1T.h"
-#elif defined ( USE_MDR1986VE3 )
-	#include "MDR1986VE3.h"
+// <o> Legacy support definition
+// <i> Specifies level of legacy support. If legacy support is not needed, outcomment this definition or change default value.
+// <i> Support for the legacy version is removed after 2 updates.
+// <i> Default value for version 2.0.x: 153 - to support previous version (1.5.3)
+#define MDR_LEGACY_SUPPORT 202
+
+
+#if (defined(USE_MDR1986VE9x) || defined (USE_MDR1901VC1T))
+// <h> JTAG pins protection for MCU MDR1986VE9x and MDR1901VC1T
+// <i> Prevents RTXT and OE bits setting for JTAG when doing GPIO read-modify-write
+// <i> Uncomment the definition(s) below to define used JTAG port(s).
+// <i> Leave all commented/unchecked if there is no GPIO pins combined with JTAG.
+
+// <c> JTAG_A pins protection
+     //#define USE_JTAG_A
+// </c>
+
+// <c> JTAG_B pins protection
+    #define USE_JTAG_B
+// </c>
+// </h>
 #endif
 
-/* Uncomment the line(s) below to define used JTAG port(s). Leave all commented
- * if there is no JTAG ports */
-#if (((!defined(USE_MDR1986VE3)) || (!defined(USE_MDR1986VE1T))) && (defined(USE_MDR1986VE9x)))
-/* #define USE_JTAG_A */
- #define USE_JTAG_B 
-#endif
 
-/* Target system parameters */
-/* RST_CLK generators frequencies in HZ */
+// <h> Target system parameters
+// <h> RST_CLK generators frequencies
+// <o> HSI clock value [Hz]
+// <i> Default: 8000000 (8MHz)
 #define HSI_Value       ((uint32_t)8000000)
+// <o> HSE clock value [Hz]
+// <i> Default: 8000000 (8MHz)
 #define HSE_Value       ((uint32_t)16000000)
+// <o> HSE2 clock value [Hz] for MDR1986VE1 and MDR1986VE3
+// <i> Default: 25000000 (25MHz)
 #define HSE2_Value      ((uint32_t)25000000)
+// <o> LSI clock value [Hz]
+// <i> Default: 40000 (40kHz)
 #define LSI_Value       ((uint32_t)40000)
+// <o> LSE clock value [Hz]
+// <i> Default: 32768 (32.768kHz)
 #define LSE_Value       ((uint32_t)32768)
+// </h>
 
-/* RST_CLK frequencies startup timeouts settings */
+
+// <h> RST_CLK frequencies startup timeouts settings
+// <o> HSE timeout startup value
+// <i> Default: 0x0600
 #define HSEonTimeOut    ((uint16_t)0x0600)
-#define HSE2onTimeOut	((uint16_t)0x8000)
+// <o> HSE2 timeout startup value for MCU MDR1986VE1 and MDR1986VE3
+// <i> Default: 0x8000
+#define HSE2onTimeOut   ((uint16_t)0x8000)
+// <o> LSE timeout startup value
+// <i> Default: 0x0600
 #define LSEonTimeOut    ((uint16_t)0x0600)
+// <o> HSI timeout startup value
+// <i> Default: 0x0600
 #define HSIonTimeOut    ((uint16_t)0x0600)
+// <o> LSI timeout startup value
+// <i> Default: 0x0600
 #define LSIonTimeOut    ((uint16_t)0x0600)
+// <o> PLLCPU timeout startup value
+// <i> Default: 0x0600
 #define PLLCPUonTimeOut ((uint16_t)0x0600)
+// <o> PLLUSB timeout startup value
+// <i> Default: 0x0600
 #define PLLUSBonTimeOut ((uint16_t)0x0600)
+// <o> PLLDSP timeout startup value for MCU MDR1901VC1
+// <i> Default: 0x0600
+#define PLLDSPonTimeOut ((uint16_t)0x0600)
+// </h>
 
+// <o> EEPROM controller freq [MHz]
+ //<i> Default: 8MHz
 #define FLASH_PROG_FREQ_MHZ     (8.0)
-/* Use debug uart */
-#define _USE_DEBUG_UART_
+// </h>
+
+
+// <h> Controller blocks settings
+// <h> DMA configuration parameters
+// <o.0..5> Number of DMA channels to use
+// <i> This parameter is in range 1..32
+// <i> Default: 32
+#define DMA_Channels_Number   32
+
+// <o> Alternate Control Data Structure Usage
+//   <0=> 0: DMA_ALternateDataDisabled
+//   <1=> 1: DMA_ALternateDataEnabled
+// <i> Default: 1 (DMA_ALternateDataEnabled)
+#define DMA_AlternateData     1
+// </h>
+
+// </h>
+
+// <h> Examples settings
+// <c2> Use debug UART
+// <i> Definition used in examples
+// <i> Default: commented/not defined
+    //#define _USE_DEBUG_UART_
+// </c>
 
 #if defined (_USE_DEBUG_UART_)
 
 #if defined (USE_MDR1986VE3)
-	#define DEBUG_UART 					MDR_UART2
-	#define DEBUG_UART_PORT				MDR_PORTD
-	#define DEBUG_UART_PINS				(PORT_Pin_13 | PORT_Pin_14)
-	#define DEBUG_UART_PINS_FUNCTION	PORT_FUNC_MAIN
-	#define DEBUG_BAUD_RATE				230400
+    #define DEBUG_UART                  MDR_UART2
+    #define DEBUG_UART_PORT             MDR_PORTD
+    #define DEBUG_UART_PINS             (PORT_Pin_13 | PORT_Pin_14)
+    #define DEBUG_UART_PINS_FUNCTION    PORT_FUNC_MAIN
+    #define DEBUG_BAUD_RATE             115200
 #elif defined (USE_MDR1986VE1T)
-	#define DEBUG_UART 					MDR_UART1
-	#define DEBUG_UART_PORT				MDR_PORTC
-	#define DEBUG_UART_PINS				(PORT_Pin_3 | PORT_Pin_4)
-	#define DEBUG_UART_PINS_FUNCTION	PORT_FUNC_MAIN
-	#define DEBUG_BAUD_RATE				230400
+    #define DEBUG_UART                  MDR_UART1
+    #define DEBUG_UART_PORT             MDR_PORTC
+    #define DEBUG_UART_PINS             (PORT_Pin_3 | PORT_Pin_4)
+    #define DEBUG_UART_PINS_FUNCTION    PORT_FUNC_MAIN
+    #define DEBUG_BAUD_RATE             115200
 #elif defined (USE_MDR1986VE9x)
-	#define DEBUG_UART 					MDR_UART2
-	#define DEBUG_UART_PORT				MDR_PORTF
-	#define DEBUG_UART_PINS				(PORT_Pin_0 | PORT_Pin_1)
-	#define DEBUG_UART_PINS_FUNCTION	PORT_FUNC_OVERRID
-	#define DEBUG_BAUD_RATE				230400
+    #define DEBUG_UART                  MDR_UART2
+    #define DEBUG_UART_PORT             MDR_PORTF
+    #define DEBUG_UART_PINS             (PORT_Pin_0 | PORT_Pin_1)
+    #define DEBUG_UART_PINS_FUNCTION    PORT_FUNC_OVERRID
+    #define DEBUG_BAUD_RATE             115200
+#elif defined (USE_MDR1901VC1T)
+    #define DEBUG_UART                  MDR_UART3
+    #define DEBUG_UART_PORT             MDR_PORTF
+    #define DEBUG_UART_PINS             (PORT_Pin_0 | PORT_Pin_1)
+    #define DEBUG_UART_PINS_FUNCTION    PORT_FUNC_ALTER
+    #define DEBUG_BAUD_RATE             115200
 #endif
-
-//#define PRINTF_FORMAT_FULL
-//#define PRINTF_FORMAT_LARGE
 
 #endif /* #if defined (_USE_DEBUG_UART_) */
 
-#if defined ( USE_MDR1986VE3 ) || defined ( USE_MDR1986VE1T )
-	#define MIL_STD_1553_TERMINAL_ADDRESS	0x01
+
+#if defined (USE_MDR1986VE3) || defined (USE_MDR1986VE1T)
+    // <o> MIL STD 1553 terminal address for MDR1986VE1 and MDR1986VE3
+    // <i> Default: 0x01
+    #define MIL_STD_1553_TERMINAL_ADDRESS    0x01
 #endif /* #if defined ( USE_MDR1986VE3 ) || defined ( USE_MDR1986VE1T ) */
 
-/* RTC configuration parameters */
-#define RTC_CalibratorValue   	0
-#define RTC_PRESCALER_VALUE		32768
 
-/* DMA configuration parameters */
-/* Number of DMA channels to use */
-#define DMA_Channels_Number   32          /* This parameter is in range 1..32 */
+// <h> RTC configuration parameters
+// <o> RTC calibration value
+// <i> Default: 0
+#define RTC_CalibratorValue     0
+// <o> RTC prescaler value
+// <i> Default: 32768
+#define RTC_PRESCALER_VALUE     32768
+// </h>
 
-/* Alternate Control Data Structure Usage */
-/* This parameter can be a value of:
-    0 = DMA_ALternateDataDisabled;
-    1 = DMA_ALternateDataEnabled; */
-/*#define DMA_AlternateData   0 */          /* DMA_AlternateDataDisabled */
-#define DMA_AlternateData   1             /* DMA_AlternateDataEnabled */
+/* ---------------------- USB configuration parameters -----------------------*/
+/* Supported USB Device Classes */
+#define USB_DEVICE_CLASS_CDC 2
 
-/* USB configuration parameters ----------------------------------------------*/
 /* Uncomment one of the lines below to select the Device Class. Leave all commented
  * if the desired device class is not currently supported by USB library */
 #define USB_DEVICE_CLASS  USB_DEVICE_CLASS_CDC
 
 /* USB Device management */
 /* Uncomment the line below to enable appropriate functionality. */
-/* #define USB_REMOTE_WAKEUP_SUPPORTED */
-/* #define USB_SELF_POWERED_SUPPORTED */
+// #define USB_REMOTE_WAKEUP_SUPPORTED
+// #define USB_SELF_POWERED_SUPPORTED
 
 /* Uncomment the line below to let the library provide USB interrupt handler.
  * Leave this line commented if you are willing to implement the handler yourself. */
@@ -154,12 +258,12 @@
 
 /* USB CDC management */
 /* Uncomment the lines below to enable appropriate functionality. */
-/* #define USB_CDC_STATE_REPORTING_SUPPORTED */
-/* #define USB_CDC_ENCAPSULATION_SUPPORTED */
-/* #define USB_CDC_COMM_FEATURE_SUPPORTED */
+//#define USB_CDC_STATE_REPORTING_SUPPORTED
+//#define USB_CDC_ENCAPSULATION_SUPPORTED
+//#define USB_CDC_COMM_FEATURE_SUPPORTED
 #define USB_CDC_LINE_CODING_SUPPORTED
-/* #define USB_CDC_CONTROL_LINE_STATE_SUPPORTED */
-/* #define USB_CDC_LINE_BREAK_SUPPORTED */
+//#define USB_CDC_CONTROL_LINE_STATE_SUPPORTED
+//#define USB_CDC_LINE_BREAK_SUPPORTED
 
 /* VCOM Echo example flags */
 
@@ -172,59 +276,72 @@
  * packets and send/receive byte counters (for debug purposes). */
 //#define USB_DEBUG_PROTO
 
-/* Known errors workaround control -------------------------------------------*/
-/* MDR32F9Qx Series Errata Notice, Error 0002 */
+// </h>
+
+
+// <h> Known errors workaround control
+// <c2> MDR1986VE9x Series Errata Notice, Error 0002
+// <i> CAN error workaround
+// <i> Default: not commented/defined
 #define WORKAROUND_MDR32F9QX_ERROR_0002
+// </c>
+// </h>
 
-/* Parameter run-time check support ------------------------------------------*/
+#if defined (__ICCARM__)
+    #define __attribute__(name_section)
+    #if defined (USE_MDR1986VE3) || defined (USE_MDR1986VE1T)
+        #pragma section = "EXECUTABLE_MEMORY_SECTION"
+        #define IAR_SECTION(section) @ section
+    #elif defined (USE_MDR1986VE9x)
+        #define IAR_SECTION(section)
+    #endif
+#endif
+#if defined (__CMCARM__)
+    #define __attribute__(name_section)
+    #define IAR_SECTION(section)
+#endif
+#if defined (__ARMCC_VERSION)
+    #define IAR_SECTION(section)
+#endif
 
-/* Select one of the following values of USE_ASSERT_INFO macro to control
-   parameter checking in the Standard Peripheral Library drivers:
-     0 - no parameter checks ("assert_param" macro is disabled);
-     1 - check enabled, source file ID and line number are available;
-     2 - check enabled, source file ID, line number and checking expression
-         (as string) are available (increased code size).
-*/
- #define USE_ASSERT_INFO    0 
-/* #define USE_ASSERT_INFO    1 */
-/* #define USE_ASSERT_INFO    2 */
+
+// <h> Parameter run-time check support
+
+// <o> Paramater checking level
+//   <0=> 0: no parameter checks ("assert_param" macro is disabled)
+//   <1=> 1: check enabled, source file ID and line number are available
+//   <2=> 2: check enabled, source file ID, line number and checking expression (as string) are available (increased code size)
+// <i> Default: 0 ("assert_param" macro is disabled)
+#define USE_ASSERT_INFO    0
 
 /**
   * @brief  The assert_param macro is used for function's parameters check.
-  * @param  expr: If expr is false, it calls assert_failed user's function
-  *   which gets the source file ID (see MDR32F9Qx_lib.h), line number and
-  *   expression text (if USE_ASSERT_INFO == 2) of the call that failed. That
-  *   function should not return. If expr is true, nothing is done.
+  * @param  expr: If expr is false, it calls assert_failed user's function which 
+  *         reports the name of the source file, source line number
+  *         and expression text (if USE_ASSERT_INFO == 2) of the call that failed.
+  *         That function should not return. If expr is true, nothing is done.
   * @retval None
   */
 #if (USE_ASSERT_INFO == 0)
-  #define assert_param(expr) ((void)0)
+    #define assert_param(expr) ((void)0U)
 #elif (USE_ASSERT_INFO == 1)
-  #define assert_param(expr) ((expr) ? (void)0 : assert_failed(ASSERT_INFO_FILE_ID, __LINE__))
-  void assert_failed(uint32_t file_id, uint32_t line);
+    #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+    void assert_failed(uint8_t* file, uint32_t line);
 #elif (USE_ASSERT_INFO == 2)
-  #define assert_param(expr) ((expr) ? (void)0 : assert_failed(ASSERT_INFO_FILE_ID, __LINE__, #expr))
-  void assert_failed(uint32_t file_id, uint32_t line, const uint8_t* expr);
+    #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__, (const uint8_t*) #expr))
+    void assert_failed(uint8_t* file, uint32_t line, const uint8_t* expr);
 #else
-  #error "Unsupported USE_ASSERT_INFO level"
+    #error "Unsupported USE_ASSERT_INFO level"
 #endif /* USE_ASSERT_INFO */
 
-#if defined (__ICCARM__)
-	#define __attribute__(name_section)
-	#if defined (USE_MDR1986VE3) || defined (USE_MDR1986VE1T)
-		#pragma section = "EXECUTABLE_MEMORY_SECTION"
-		#define IAR_SECTION(section) @ section
-	#elif defined (USE_MDR1986VE9x)
-		#define IAR_SECTION(section)
-	#endif
-#endif
-#if defined (__CMCARM__)
-		#define __attribute__(name_section)
-		#define IAR_SECTION(section)
+// </h>
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* __MDR32F9Qx_CONFIG_H */
 
-/******************* (C) COPYRIGHT 2011 Milandr *********************************
+/*********************** (C) COPYRIGHT 2021 Milandr ****************************
 *
 * END OF FILE MDR32F9Qx_config.h */
--- source/type_custom.h
+++ source/type_custom.h
@@ -2,7 +2,6 @@
 #define __TYPE_CUSTOM_H__
 
 #include "MDR32F9Qx_config.h"
-#include "MDR32F9Qx_board.h"
 #include "MDR32F9Qx_rst_clk.h"
 #include "MDR32F9Qx_uart.h"
 #include "MDR32F9Qx_port.h"
